33a056266d8461131a45c5e7a04469d238471ab4
Kannav Dhawan <k4dhawan@uwaterloo.ca>
2020-07-18 09:22:53 +0000

    push

--- train_RNN.py
+++ train_RNN.py
@@ -15,104 +15,154 @@ from tensorflow.keras.layers import LSTM, Dense, Dropout
 
 from matplotlib import pyplot as plt
 import tensorflow as tf
-tf.random.set_seed(1337)
+
+tf.random.set_seed(1337) # setting seed 
 
 #Loading the dataset
+def load_data(path):
+    """
 
-# # path='https://raw.githubusercontent.com/kannavdhawan/Time_series_stocks_LSTM/master/q2_dataset.csv?token=AHMAKJAQPICJFWARH35B4US7DEIJE'
-# path=os.path.join("data/","q2_dataset.csv")
-# data=pd.read_csv(path)
+    Loads the dataset with stock prices from csv.
 
-# print(data.head(3))
-# # Dropping the Close/Last column 
-# data_no_close=data.drop([" Close/Last"],axis=1)
-# data_no_close.head(3)
-# data_no_close['Date']=pd.to_datetime(data_no_close['Date'])
+    Args:
+        path(os path)
 
-# print(data_no_close.head(3))
+    Arguments:
+        path(os path): path to the csv from the data folder
 
-# data_no_close=data_no_close.sort_values('Date')
+    Returns: 
+        data_no_close: A numpy array with float values with no 'close' column.
 
-# print(data_no_close)
+    """
+    print("Loading data..")
 
-# data_no_close=data_no_close.reset_index(drop=True)
+    # path='https://raw.githubusercontent.com/kannavdhawan/Time_series_stocks_LSTM/master/q2_dataset.csv?token=AHMAKJAQPICJFWARH35B4US7DEIJE'
 
-# print(data_no_close.head(3))
+    data=pd.read_csv(path)
+    # print("Checking head..:\n",data.head(3))
 
+    data_no_close=data.drop([" Close/Last"],axis=1)             # Dropping the Close/Last column 
+    # print("No close ds test..:\n",data_no_close.head(3))
 
-# data_no_close=data_no_close.iloc[:,1:].values.astype('float32')
+    data_no_close['Date']=pd.to_datetime(data_no_close['Date']) # converting date from str to datetime
+    # print("Checking ds..:\n",data_no_close.head(3))
 
-# print("rand check: ",data_no_close[0])
-# print(data_no_close.shape)
+    data_no_close=data_no_close.sort_values('Date')             # Sorting df by date
+    # print("sorted date ds..:\n",data_no_close)
 
+    data_no_close=data_no_close.reset_index(drop=True)          # Reseting the reversed index after sorting.
+    # print("Reset index ds..:\n",data_no_close.head(3))
 
-# def feature_gen(data_values,size):
-#   features=[]
-#   targets=[]
-#   old_days=size
-#   for current_day in range(old_days,len(data_values)):
-#     features.append(data_values[current_day-old_days:current_day,:])  #[1259*(3,4)shape]
-#     targets.append(data_values[current_day][1])
-#   return features,targets
-# features,targets=feature_gen(data_no_close,3)
+    data_no_close=data_no_close.iloc[:,1:].values.astype('float32')     #converting into np array and 'float32'.  {Already floats!~Volume}
+    # print("np array floats..:\n",data_no_close[0])
+    # print("Shape np array=> ",data_no_close.shape)
 
-# print(features[0])
-# print(features[1])
-# print(targets[0])
-# print("shape check features: ",np.asarray(features).shape)
-# print("shape check target: ",np.asarray(targets).shape)
-
-# # shuffling the dataset
-# zipped_f_t=list(zip(features,targets))
-# random.shuffle(zipped_f_t)
-# len(zipped_f_t)
+    return data_no_close
 
 
-# # splitting the dataset into train and test
-# tr_sz=int(0.70*len(zipped_f_t))
+# Generating the features
+def feature_gen(data_values,size):
+    """
+    Generates the features for lstm with the required window size for time series input. 
 
-# train, test = zipped_f_t[:tr_sz], zipped_f_t[tr_sz:]
+    Args:
+        data_values(np array)
+        size(int)
 
-# print(train[0])
+    Arguments:
+        data_values(np array): numpy array returned by load_data with float values. 
+        size(int): window size
+    
+    Returns: 
+        features(list): [1256, 3, 4]
+        targets(list): [1256]
 
+    """
+    features=[]
+    targets=[]
+    old_days=size
+    for current_day in range(old_days,len(data_values)):
+        features.append(data_values[current_day-old_days:current_day,:])  #[1259*(3,4)shape]
+        targets.append(data_values[current_day][1])
 
-# print(len(train))
-# print(len(test))
+    # print("type(features):",type(features))           #list
+    # print("type(features[0])",type(features[0]))      #numpy array
+    # print("shape check features: ",np.asarray(features).shape) # (1256, 3, 4)
+    # print("shape check target: ",np.asarray(targets).shape)    # (1256,)
+    return features,targets
 
-# # flattening the arrays stored in lists to save in a csv file 
-# #Each feature set below is a tuple with features of shape (3,4) and an int value for target..
-# X_train=pd.DataFrame([feature_set[0].ravel() for feature_set in train])
-# X_test=pd.DataFrame([feature_set[0].ravel() for feature_set in test])
-# y_train=pd.DataFrame([feature_set[1] for feature_set in train])
-# y_test=pd.DataFrame([feature_set[1] for feature_set in test])
+def preprocess(features,targets):
+    
+    # shuffling the dataset
+    zipped_f_t=list(zip(features,targets))
+    random.shuffle(zipped_f_t)                           #shuffling the zipped object with features and labels
+    tr_sz=int(0.70*len(zipped_f_t))                      # getting size for splitting the dataset into train and test
+    train,test = zipped_f_t[:tr_sz], zipped_f_t[tr_sz:]  # splitting the zipped object list
+    # print(len(train))
+    # print(len(test))
 
-# # print((X_train).head(2))
-# # print((X_test).head(2))
-# # print((y_train).head(2))
-# # print((y_test).head(2))
+    
+    """
+    ** train,test : list of truple of numpy array of shape (None,3,4) and int target.
 
-# #Making dataframes 
-# train=X_train.copy()
-# train['Target']=y_train
-# test=X_test.copy()
-# test['Target']=y_test
+    ** This preprocessing is done to store the data in a csv file ..
+    
+        * Each feature set below is a tuple with features of shape (3,4)
+            and an int value for target at first index.
+        * we flatten the features at 0th index from shae (3,4) to 12 for each element/input. 
+        
+    """
+    X_train=pd.DataFrame([feature_set[0].ravel() for feature_set in train])
+    X_test=pd.DataFrame([feature_set[0].ravel() for feature_set in test])
+    y_train=pd.DataFrame([feature_set[1] for feature_set in train])
+    y_test=pd.DataFrame([feature_set[1] for feature_set in test])
 
-# print(train.head(3))
-# print(test.head(3))
-# train.to_csv(os.path.join("data/","train_data_RNN.csv"))
-# test.to_csv(os.path.join("data/","test_data_RNN.csv"))
+    # print((X_train).head(2))
+    # print((X_test).head(2))
+    # print((y_train).head(2))
+    # print((y_test).head(2))
+
+    # Creating dataframes 
+
+    train=X_train.copy()        
+    train['Target']=y_train     # concatinating labels
+    
+    test=X_test.copy()
+    test['Target']=y_test       # concatinating labels
+
+    # print(train.head(3))
+    # print(test.head(3))
+
+    train.to_csv(os.path.join("data/","train_data_RNN.csv")) # saving csv of train and test in data/
+    test.to_csv(os.path.join("data/","test_data_RNN.csv"))
+
 
 """
-Loading dataset 
+//CALLS FOR CREATING TIME SERIES DATASET//
 """
-train=pd.read_csv(os.path.join("data/","train_data_RNN.csv"),index_col=[0])
+# Loading dataset from csv
+data_no_close=load_data(os.path.join("data/","q2_dataset.csv"))
 
-print(train.head())
+# Generating features
+win_size=3
+features,targets=feature_gen(data_no_close,win_size)
 
-#separating data and labels
-X_train,y_train=train.iloc[:,:-1],train['Target']
-print(X_train.head())
-print(y_train.head()) 
+# Preprocessing and saving preprocessed dataset 
+preprocess(features,targets)
+
+
+def dataset():
+    """
+    Loading dataset 
+    """
+    train=pd.read_csv(os.path.join("data/","train_data_RNN.csv"),index_col=[0])
+
+    print(train.head())
+
+    #separating data and labels
+    X_train,y_train=train.iloc[:,:-1],train['Target']
+    print(X_train.head())
+    print(y_train.head()) 
 
 
 #scaling 
